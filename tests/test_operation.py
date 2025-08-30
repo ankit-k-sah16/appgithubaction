910ad29ce6fcdab9f2361583007fb09f5ecd8db0
ankit-k-sah16 <ankitkumar040920@gmail.com>
2025-08-30 18:40:14 +0530

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,10 +1,10 @@
 from src.math_operation import add,subtract,multiply,divide
 
 def test_add():
-    assert add(2,3)==5
-    assert add(34+24)==68
+    assert add(2 + 3)==5
+    assert add(34 + 24)==68
 
 def test_subtract():
-    assert subtract(200-50)==150
-    assert subtract(43-43)==0
+    assert subtract(200 - 50)==150
+    assert subtract(43 - 43)==0
 
